ad6ee8f4e9414659f7b575a6e6876c39dc923fef
Jesse Farinacci <jieryn@gmail.com>
2011-09-07 22:09:57 -0400

    adding license to all source files, format code

--- src/main/java/hudson/plugins/saferestart/SafeRestartManagementLink.java
+++ src/main/java/hudson/plugins/saferestart/SafeRestartManagementLink.java
@@ -1,47 +1,71 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2010-2011, Seiji Sogabe, Jesse Farinacci
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package hudson.plugins.saferestart;
 
 import hudson.Extension;
-import hudson.model.Hudson;
 import hudson.model.ManagementLink;
+import hudson.model.Hudson;
+
 import org.kohsuke.stapler.Stapler;
 import org.kohsuke.stapler.StaplerRequest;
 
 /**
- * ManagementLink for SafeRestart.
- * added restrat link to system admin.
+ * ManagementLink for SafeRestart. added restrat link to system admin.
+ * 
  * @author Seiji Sogabe
  */
 @Extension
 public class SafeRestartManagementLink extends ManagementLink {
+  @Override
+  public String getDescription() {
+    return Messages.SafeRestartManagementLink_description();
+  }
 
-    @Override
-    public String getDescription() {
-        return Messages.SafeRestartManagementLink_description();
-    }
+  public String getDisplayName() {
+    return Messages.SafeRestartManagementLink_displayName();
+  }
 
-    @Override
-    public String getIconFileName() {
-        return "/plugin/" + Contsants.ID + "/images/48x48/" + Contsants.ICON;
-    }
+  @Override
+  public String getIconFileName() {
+    return "/plugin/" + Contsants.ID + "/images/48x48/" + Contsants.ICON;
+  }
 
-    /**
-     * {@inheritDoc}
-     *
-     * <p>
-     * In case of {@link ManagementLink}, this value is put straight into
-     * the href attribute,
-     * so relative paths are interpreted against the root {@link Hudson} object.
-     */
-    @Override
-    public String getUrlName() {
-        final StaplerRequest req = Stapler.getCurrentRequest();
-        if (req != null) {
-            return req.getContextPath() + Contsants.RESTART_URL;
-        }
-        return Contsants.RESTART_URL;
+  /**
+   * {@inheritDoc}
+   * 
+   * <p>
+   * In case of {@link ManagementLink}, this value is put straight into the href
+   * attribute, so relative paths are interpreted against the root
+   * {@link Hudson} object.
+   */
+  @Override
+  public String getUrlName() {
+    final StaplerRequest req = Stapler.getCurrentRequest();
+    if (req != null) {
+      return req.getContextPath() + Contsants.RESTART_URL;
     }
-
-    public String getDisplayName() {
-        return Messages.SafeRestartManagementLink_displayName();
-    }
+    return Contsants.RESTART_URL;
+  }
 }
--- src/main/java/hudson/plugins/saferestart/SafeRestartRootAction.java
+++ src/main/java/hudson/plugins/saferestart/SafeRestartRootAction.java
@@ -1,39 +1,56 @@
+/*
+ * The MIT License
+ * 
+ * Copyright (c) 2010-2011, Seiji Sogabe, Jesse Farinacci
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package hudson.plugins.saferestart;
 
 import hudson.Extension;
-import hudson.model.Hudson;
 import hudson.model.RootAction;
+import hudson.model.Hudson;
 
 /**
- * Action for SafeResatrt.
- * added restart link to left-side panel.
+ * Action for SafeResatrt. added restart link to left-side panel.
+ * 
  * @author Seiji Sogabe
  */
 @Extension
 public class SafeRestartRootAction implements RootAction {
+  public String getDisplayName() {
+    return Messages.SafeRestartRootAction_displayName();
+  }
 
-    public String getDisplayName() {
-        return Messages.SafeRestartRootAction_displayName();
+  public String getIconFileName() {
+    if (!hasAdministerPermission()) {
+      return null;
     }
+    return "/plugin/" + Contsants.ID + "/images/24x24/" + Contsants.ICON;
+  }
 
-    public String getIconFileName() {
-        if (!hasAdministerPermission()) {
-            return null;
-        }
-        return "/plugin/" + Contsants.ID + "/images/24x24/" + Contsants.ICON;
-    }
+  public String getUrlName() {
+    return Contsants.RESTART_URL;
+  }
 
-    private boolean hasAdministerPermission() {
-        return Hudson.getInstance().hasPermission(Hudson.ADMINISTER);
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * This actions always starts from the context directly,
-     * so prefix {@link JobConfigHistoryConsts#URLNAME} with a slash.
-     */
-    public String getUrlName() {
-        return Contsants.RESTART_URL;
-    }
+  private boolean hasAdministerPermission() {
+    return Hudson.getInstance().hasPermission(Hudson.ADMINISTER);
+  }
 }
